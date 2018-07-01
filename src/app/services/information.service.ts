f15c672de49e95c595890a46db11709029148ccd
Duvan Lopez <stevenbarcalop@gmail.com>
2018-06-30 20:49:40 -0500

    Add animations and service Firebase

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -7,14 +7,31 @@ import { Http } from "@angular/http";
 export class InformationService {
 
   info:any = {};
+  aboutTeamData:any = {};
   cargada:boolean = false;
+  readAbout:boolean = false;
 
   constructor(public http:Http) {
-      this.http.get("assets/data/info.pag.json")
-                .subscribe(data =>{
-                  console.log(data.json());
-                  this.info = data.json();
-                  this.cargada = true;
-                })
+      this.readInfoData();
+      this.readAboutData();
    }
+
+   public readInfoData(){
+    this.http.get("assets/data/info.pag.json")
+        .subscribe(data =>{
+          //console.log(data.json());
+          this.info = data.json();
+          this.cargada = true;
+        });
+   }
+
+   public readAboutData(){
+    this.http.get("https://cursoangular-4c4fa.firebaseio.com/equipo.json")
+      .subscribe(data =>{
+        console.log(data.json());
+        this.aboutTeamData = data.json();
+        this.readAbout = true;
+      });
+   }
+
 }
